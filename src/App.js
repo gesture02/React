7f67eacaa561e6ee3238e3d5ee8fda81934c6fa3
gesture02 <gesture02@gmail.com>
2021-11-22 20:16:51 +0900

    useState

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import Sub from './Sub';
 
 // 0. react 엔진 - 데이터변경감지해서 UI그려줌
 // 1. 실행과정(index.html) : SinglePageApplication(SPA)
@@ -33,15 +35,51 @@ import './App.css';
 //   );
 // }
 
+// function App() {
+//   //let number = 1; //상태값(x)
+//   const [number, setNumber] = useState(1); // react hooks lib 상태값(o)
+//   const add = () => {
+//     setNumber(number + 1); // 리액트한테 number값 변경할게 라고 요청
+//     console.log('add', number);
+//   };
+//   //렌더링 시점 = 상태값 변경(setNumber 호출할 때)
+//   return (
+//     <div>
+//       <h1>숫자 : {number}</h1>
+//       <button onClick={add}>더하기</button>
+//       <Sub />
+//     </div>
+//   );
+// }
+
 function App() {
-  let list = [1, 2, 3];
+  console.log('app실행');
+  const [users, setUsers] = useState([
+    // 레퍼런스가 변경되어야 동작한다. =====> 깊은복사
+    { id: 1, name: '홍길동' },
+    { id: 2, name: '홍길일' },
+    { id: 3, name: '홍길이' },
+    { id: 4, name: '홍길삼' },
+  ]);
+  const download = () => {
+    let sample = [
+      { id: 1, name: '홍길동' },
+      { id: 2, name: '홍길일' },
+      { id: 3, name: '홍길이' },
+      { id: 4, name: '홍길삼' },
+    ];
+    //레퍼런스가 다르면 렌더링 다시됨
+    setUsers(sample); //[...sample] 과의 차이 : 둘다 레퍼런스는 다르다, 그래서 리렌더링은 됨
+    // 차이점은 [...sample]은 깊은복사이기 때문에 무조건 리렌더링된다.
+  };
   return (
     <div>
-      <div>
-        {list.map((n) => (
-          <h1>{n}</h1>
-        ))}
-      </div>
+      <button onClick={download}>다운로드</button>
+      {users.map((user) => (
+        <h1>
+          {user.id}, {user.name}
+        </h1>
+      ))}
     </div>
   );
 }
---
+++ src/Sub.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Sub = () => {
+  //자식을 컴포넌트로 뽑아놓으면 리턴을 제어가능함
+  return (
+    <div>
+      <h1>Sub입니다</h1>
+    </div>
+  );
+};
+
+export default Sub;
